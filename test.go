8999877a02698a981473cc540f4e6ce8922bdc56
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-02-13 16:44:40 +0530

    test.go for testing purposes

---
+++ test.go
@@ -0,0 +1,7 @@
+package myshoo
+
+// for dev purpose
+// don't remove the call for this from main.go until the project is complete
+// (to avoid frequent changes in main.go file for testing purpose)
+func Test() {
+}
